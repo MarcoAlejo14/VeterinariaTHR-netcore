54d83d6cbd2fe04a591af224cf22cb4b9652e84d
MarcoAlejo14 <malejoa@isise.edu.pe>
2026-02-11 18:28:36 -0500

    Se establecio la conexion con la base de datos

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using VeterinariaAppWeb.Models;
+
+
+namespace VeterinariaAppWeb.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly string cadenaConexion = @"Server=localhost\SQLEXPRESS;database=veterinaria;user id=sa;password=sqladmin;TrustServerCertificate=true";
+        public IActionResult Index()
+        {
+            var listado = obtenerProdutos();
+            return View(listado);
+        }
+
+        #region . Métodos privados .
+        private List<Producto>  obtenerProdutos()
+        {
+            var listado = new List<Producto>();
+            using (var conexion= new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+            }
+                return listado;
+        }
+
+        #endregion
+    }
+}
--- obj/Debug/net8.0/VeterinariaAppWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/VeterinariaAppWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VeterinariaAppWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+612acdc05c916c941e09ddf9beb023f4aba49565")]
 [assembly: System.Reflection.AssemblyProductAttribute("VeterinariaAppWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VeterinariaAppWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
